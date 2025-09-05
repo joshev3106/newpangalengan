453771753b593809af2230732d040ad9775b355b
joshev3106 <joshevgedoan3106@gmail.com>
2025-09-05 23:03:40 +0800

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,10 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-<<<<<<< HEAD
-=======
             $table->enum('role', ['admin', 'user'])->default('user');
->>>>>>> 5e72e353cbdc1c231dc14cc870b4f7596b7ae72b
             $table->rememberToken();
             $table->timestamps();
         });
